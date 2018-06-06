eb3ba1409c65bde6969e94e7f833e873defc6dec
Boyd Ndonga <boydbugle@gmail.com>
2018-06-06 16:49:30 +0300

    create ArrayAdapters

--- app/src/main/java/com/example/ndonga/fitnesstracker/FitnessActivity.java
+++ app/src/main/java/com/example/ndonga/fitnesstracker/FitnessActivity.java
@@ -3,6 +3,7 @@ package com.example.ndonga.fitnesstracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class FitnessActivity extends AppCompatActivity {
--- app/src/main/java/com/example/ndonga/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/ndonga/fitnesstracker/MainActivity.java
@@ -6,35 +6,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
+//    public static final String TAG = MainActivity.class.getSimpleName();
     private EditText mTrackFitnessInput;
     private Button mTrackFitnessButton;
     private TextView mHomeTextView;
+    private ListView mMainListView;
+    private String[] fitTracks = new String[] {"Physical", "Educational", "Social", "Mentorship"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mTrackFitnessInput = (EditText) findViewById(R.id.trackFitnessInput);
+        mMainListView = (ListView) findViewById(R.id.mainListView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, fitTracks);
+        mMainListView.setAdapter(adapter);
+        mMainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String track = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, track, Toast.LENGTH_LONG).show();
+            }
+        });
+
         mHomeTextView = (TextView) findViewById(R.id.homeTextView);
         Typeface awesomeFont = Typeface.createFromAsset(getAssets(), "fonts/KaushanScript-Regular.otf");
         mHomeTextView.setTypeface(awesomeFont);
+
         mTrackFitnessButton = (Button) findViewById(R.id.trackFitnessButton);
-            mTrackFitnessButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
+        mTrackFitnessButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
 //                    Toast.makeText(MainActivity.this, "flash message", Toast.LENGTH_LONG).show();
-                    String track = mTrackFitnessInput.getText().toString();
-                    Log.d(TAG, track);
-                    Intent intent = new Intent(MainActivity.this, FitnessActivity.class);
-                    intent.putExtra("track", track);
-                    startActivity(intent);
-                }
-            });
+                String track = mTrackFitnessInput.getText().toString();
+//                Log.d(TAG, track);
+                Intent intent = new Intent(MainActivity.this, FitnessActivity.class);
+                intent.putExtra("track", track);
+                startActivity(intent);
+            }
+        });
     }
 }
