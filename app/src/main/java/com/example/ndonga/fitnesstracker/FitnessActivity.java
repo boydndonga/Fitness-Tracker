e9aa0eda76f1c6e95576527972fecf9471c6d56d
Boyd Ndonga <boydbugle@gmail.com>
2018-06-06 18:32:39 +0300

    create binding and refactor codeO

--- app/src/main/java/com/example/ndonga/fitnesstracker/FitnessActivity.java
+++ app/src/main/java/com/example/ndonga/fitnesstracker/FitnessActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FitnessActivity extends AppCompatActivity {
-    private TextView mTracksTextView;
+    @BindView(R.id.tracksTextView) TextView mTracksTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
-        mTracksTextView = (TextView) findViewById(R.id.tracksTextView);
+        @ButterKnife.bind(this);
         Intent intent = getIntent();
         String track = intent.getStringExtra("track");
         mTracksTextView.setText("Welcome to" + track + " Fitness");
