6df1cd1e228103de170ecbf7f0ce7999c5c8d7f4
Boyd Ndonga <boydbugle@gmail.com>
2018-06-06 18:30:40 +0300

    refactor code in MainActivity

--- app/src/main/java/com/example/ndonga/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/ndonga/fitnesstracker/MainActivity.java
@@ -18,7 +18,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-//    public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.trackFitnessInput) EditText mTrackFitnessInput;
     @BindView(R.id.trackFitnessButton) Button mTrackFitnessButton;
     @BindView(R.id.homeTextView) TextView mHomeTextView;
@@ -31,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mTrackFitnessInput = (EditText) findViewById(R.id.trackFitnessInput);
-//        mMainListView = (ListView) findViewById(R.id.mainListView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, fitTracks);
         mMainListView.setAdapter(adapter);
         mMainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -43,17 +40,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        mHomeTextView = (TextView) findViewById(R.id.homeTextView);
         Typeface awesomeFont = Typeface.createFromAsset(getAssets(), "fonts/KaushanScript-Regular.otf");
         mHomeTextView.setTypeface(awesomeFont);
 
-//        mTrackFitnessButton = (Button) findViewById(R.id.trackFitnessButton);
         mTrackFitnessButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-//                    Toast.makeText(MainActivity.this, "flash message", Toast.LENGTH_LONG).show();
                 String track = mTrackFitnessInput.getText().toString();
-//                Log.d(TAG, track);
                 Intent intent = new Intent(MainActivity.this, FitnessActivity.class);
                 intent.putExtra("track", track);
                 startActivity(intent);
