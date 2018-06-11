12c1e750985a4a92e4f4630ca88bb5c4316f8bb7
Boyd Ndonga <boydbugle@gmail.com>
2018-06-11 15:53:58 +0300

    rename xml file

--- app/src/main/java/com/example/ndonga/fitnesstracker/ImageActivity.java
+++ app/src/main/java/com/example/ndonga/fitnesstracker/ImageActivity.java
@@ -1,4 +1,36 @@
 package com.example.ndonga.fitnesstracker;
 
-public class ImageActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class ImageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+    }
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.alexunsplash,
+            R.drawable.superhero,
+            R.drawable.tower,
+            R.drawable.gohunsplash,
+    };
 }
